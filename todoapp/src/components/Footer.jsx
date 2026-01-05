ef80d0e5c7741648840143849232ea67b1d09e65
salako2929 <adegokej350@gmail.com>
2026-01-05 11:11:59 +0100

    removed space

--- todoapp/src/components/Footer.jsx
+++ todoapp/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer({ completedTodos, totalTodos }) {
   return (
     <div className={styles.footer}>
       <span className={styles.item}>Completed Todos:{completedTodos}</span>
-      <span className={styles.item}>Total Todos :{totalTodos}</span>
+      <span className={styles.item}>Total Todos:{totalTodos}</span>
     </div>
   );
 }
